6c9a33ed1231464d78c084be7f445fb1d69792f8
Luis <95424776+redluis@users.noreply.github.com>
2021-12-29 22:24:53 +0200

    v 1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
-"use strict"
+'use strict'
 
+const showContactsButton = document.querySelector('#show-contacts'),
+      modalWindow = document.querySelector('#modal-window');
 
+showContactsButton.addEventListener('click', toggleContacts);
+modalWindow.addEventListener('click', toggleContacts)
+
+function toggleContacts() {
+    modalWindow.classList.toggle('is-open');
+}
+
+new WOW().init();
